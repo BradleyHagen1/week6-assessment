3d7ad2f84d1624f6a2f1ef76eb3e9191e74cb078
Bradley Hagen <Brad.hagen94@gmail.com>
2023-01-27 11:51:55 -0800

    first commit

--- functions.test.ts
+++ functions.test.ts
@@ -3,7 +3,7 @@ const {shuffleArray} = require('./utils')
 describe('shuffleArray should', () => {
     // CODE HERE
    test('return an array', () => {
-    expect(shuffleArray([])).toEqual([])
+    expect(shuffleArray([])).toBe([])
    })
 
 
